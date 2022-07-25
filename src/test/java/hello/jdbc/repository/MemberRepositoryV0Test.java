ca9598dab7f8c05dd3de3203d8a6669379a7cf27
skahn <d1215@naver.com>
2022-07-25 22:34:55 +0900

    feat : Add crud test

---
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -0,0 +1,37 @@
+package hello.jdbc.repository;
+
+import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+class MemberRepositoryV0Test {
+    MemberRepositoryV0 repositoryV0 = new MemberRepositoryV0();
+
+    @Test
+    void crud() throws SQLException {
+        Member member = new Member("member1", 100000);
+        repositoryV0.save(member);
+
+        Member findMember = repositoryV0.findById(member.getMemberId());
+        log.info("findMember={}", member);
+        log.info("member != findMember {}", member==findMember);
+        Assertions.assertThat(findMember).isEqualTo(member);
+    }
+
+
+    public void update(String memberId, int money) {
+        String sql = "update member set money=? where member_id=?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+    }
+}
