aaab6ce0f4dc0214f5cf2623bff37c9452fe5607
skahn <d1215@naver.com>
2022-09-23 00:08:59 +0900

    feat : Add checked exception test

--- src/test/java/hello/jdbc/exception/basic/CheckedAppTest.java
+++ src/test/java/hello/jdbc/exception/basic/CheckedAppTest.java
@@ -7,6 +7,13 @@ import java.net.ConnectException;
 import java.sql.SQLException;
 
 public class CheckedAppTest {
+
+    /**
+     * 언체크드를 쓰게 되면 익셉션에 의존하게 된다.
+     * 예를 들어 ConnectionException 은 SQL Exception 인데
+     * 나중에 레파지토리를 변경하면 어떻게 될까? 당연 변경해야된다.
+     * 의존성이 발생하게 된다.
+     */
     @Test
     void cheked() {
         Controller controller = new Controller();
